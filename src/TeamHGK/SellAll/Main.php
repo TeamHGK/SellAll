a3507bcd8e2a2e0dc11082b544ed2b600d5d4284
TeamHGK <36576656+TeamHGK@users.noreply.github.com>
2018-04-21 15:22:40 +0200

    Create Main.php

---
+++ src/TeamHGK/SellAll/Main.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TeamHGK\SellALl;
+
+use pocketmine\plugin\PluginBase;
+
+class Example extends PluginBase{
+
+        public function onLoad(){
+        }
+
+        public function onEnable(){
+        }
+
+        public function onDisable(){
+        }
+}
